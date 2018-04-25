794236635927bf370fcb341f890b23f58f0d80d2
wilsonchoi1 <eewai97@hotmail.com>
2018-04-25 21:14:44 +0800

    Update IndexController.java

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -3,6 +3,7 @@ package guru.springframework.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import guru.springframework.domain.*;
 
 @Controller
 public class IndexController {
